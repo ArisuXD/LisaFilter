38f469baa4155e3d3ed160809e3ccba20acc4432
Kyouka Uzen <101930427+KyoukaUzen@users.noreply.github.com>
2022-06-25 10:30:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/alicewabot/EvaMaria.git /EvaMaria
+  git clone https://github.com/KyoukaUzen/LisaFilter.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Lisafilter
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
