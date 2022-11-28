086dc6a7a9b67b439cf45c2c2a5f788ece5d2bb0
Kyouka Uzen <101930427+KyoukaUzen@users.noreply.github.com>
2022-11-28 08:37:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/KyoukaUzen/LisaFilter.git /EvaMaria
+  git clone https://github.com/KyoukaUzen/LisaFilter.git /LisaFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lisafilter
+  git clone $UPSTREAM_REPO /LisaFilter
 fi
-cd /EvaMaria
+cd /LisaFilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
